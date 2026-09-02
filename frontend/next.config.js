ae84a3e13af84812277c7e3e38a1124cdd6951ec
Harsh Jha <harsh.jha.ug23@nsut.ac.in>
2026-09-02 20:18:47 +0530

    backend issue resolve

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NODE_ENV === 'production' ? '/api/:path*' : 'http://localhost:4000/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
